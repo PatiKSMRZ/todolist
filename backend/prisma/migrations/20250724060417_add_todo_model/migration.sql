4abdea7054c5e8767a19d4b8a61926e1e19094f3
PatiKSMRZ <patr.kusmierz@gmail.com>
2025-07-24 08:29:54 +0200

    prisma - add new model Todo

---
+++ backend/prisma/migrations/20250724060417_add_todo_model/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
